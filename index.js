85673bd5033965e7752e379bea29db13d21abcef
Chinmay Shah <chinmayjshah007@gmail.com>
2024-09-24 02:34:17 -0400

    new things for typescript added

--- index.js
+++ index.js
@@ -7,9 +7,33 @@ const menu = [
 
 const cashInRegister = 100
 const orderQueue = []
+const nextOrderId = 1
 
 function addNewPizza(pizzaObj){
     menu.push(pizzaObj)
 }
 
-function placeOrder()
+function placeOrder(pizzaName){
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = {id:nextOrderId++,pizza:selectedPizza, status:"ordered"}
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId){
+    const order = orderQueue.find(order=>order.id === orderId)
+    order.status = "completed"
+    return order
+}
+
+addNewPizza({name: "Chicken Bacon Ranch",cost:12})
+addNewPizza({name: "BBQ Chicken",cost:12})
+addNewPizza({name: "Spicy Sausage",cost:11})
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder("1")
+
+console.log("Menu:",menu)
+console.log("Cash in register:",cashInRegister)
+console.log("Order queue:",orderQueue)
